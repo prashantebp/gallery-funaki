55cc5e0854e3db42f95b1c6d763a78cba000ba45
prashant shrstha <prashant@ebpearls.com.au>
2022-08-24 15:31:17 +0545

    scss file added

---
+++ funakiDesign/assets/js/main.js
@@ -0,0 +1,19 @@
+$('.js-splide-1').length &&
+new Splide( '.js-splide-1', {
+    // perPage: 5,
+    perMove: 1,
+    gap: "29px",
+    autoWidth:true,
+    pagination: false,
+
+} ).mount();
+
+$('.js-splide-2').length &&
+new Splide( '.js-splide-2', {
+    // perPage: 5,
+    perMove: 1,
+    gap: "18px",
+    autoWidth:true,
+    pagination: false,
+
+} ).mount();
