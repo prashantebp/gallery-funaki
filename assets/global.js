663a187c604401251bdf8aadf851dbae48630b79
rajan <r.silwal@andmine.com>
2022-08-29 15:43:01 +0545

    updated

--- assets/global.js
+++ assets/global.js
@@ -943,7 +943,7 @@ if(result && input_val != 0){
 }
 
 var live_url = window.location.href;
-var result = live_url.includes('form_type=customer_contact');
+var result = live_url.includes('form_type=customer');
 var input_val = document.querySelector('#NewsletterForm--template--16207607529684__16560488181854b832').value.length;
 if(result && input_val != 0){
     const add_ele = document.createElement("p");
