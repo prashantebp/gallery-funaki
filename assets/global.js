b6c1602b7c438eb2e2afee94718e0a46f01b8399
rajan <r.silwal@andmine.com>
2022-08-30 15:25:58 +0545

    updated

--- assets/global.js
+++ assets/global.js
@@ -178,13 +178,13 @@ class QuantityInput extends HTMLElement {
     );
   }
 
-  // onButtonClick(event) {
-  //   event.preventDefault();
-  //   const previousValue = this.input.value;
+  onButtonClick(event) {
+    event.preventDefault();
+    const previousValue = this.input.value;
 
-  //   event.target.name === 'plus' ? this.input.stepUp() : this.input.stepDown();
-  //   if (previousValue !== this.input.value) this.input.dispatchEvent(this.changeEvent);
-  // }
+    event.target.name === 'plus' ? this.input.stepUp() : this.input.stepDown();
+    if (previousValue !== this.input.value) this.input.dispatchEvent(this.changeEvent);
+  }
 }
 
 customElements.define('quantity-input', QuantityInput);
