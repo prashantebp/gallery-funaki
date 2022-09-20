9115c7712c08086f6ea5d5b7860756ae1c337d99
rajan <r.silwal@andmine.com>
2022-09-20 11:46:55 +0545

    updated

--- assets/global.js
+++ assets/global.js
@@ -38,9 +38,9 @@ console.log(jQuery('.product-slider-box').slick.slideCount);
     jQuery('.product-slider-box').on('init', function(event, slick){
       jQuery(this).append('slick.slideCount');
     });
-    $('.product-slider-box li').each(function() {
-      console.log($(this));
-    })
+    $( ".product-slider-box li" ).each(function( index ) {
+      console.log( index + ": " + $( this ).text() );
+    });
 
     jQuery('.product-slider-box').slick({
       slidesToShow: 3,
