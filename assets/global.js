2ade7463e093cd7ea542870da9be05132b52e3e7
rajan <r.silwal@andmine.com>
2022-09-20 07:31:44 +0545

    updated

--- assets/global.js
+++ assets/global.js
@@ -36,20 +36,21 @@ jQuery(document).ready(function(jQuery){
 
 
 jQuery(".product-slider-box").on("init", function(event, slick){
-  console.log(slick.slideCount);
-  console.log(slick.currentSlide+1);
+  if(slick.slideCount > 3){
+    jQuery('.product-slider-box').slick({
+      slidesToShow: 3,
+      slidesToScroll: 1,
+      dots: false,
+      centerMode: false,
+      focusOnSelect: true,
+      infinite: true,
+      arrows: true,
+      prevArrow: '.fun-banner__prev-btn',
+      nextArrow: '.fun-banner__next-btn'
   });
-jQuery('.product-slider-box').slick({
-    slidesToShow: 3,
-    slidesToScroll: 1,
-    dots: false,
-    centerMode: false,
-    focusOnSelect: true,
-    infinite: true,
-    arrows: true,
-    prevArrow: '.fun-banner__prev-btn',
-    nextArrow: '.fun-banner__next-btn'
-});
+  }
+  });
+
 
   // var slickGallery = jQuery('#gallery').slick({
   //     slidesToShow: 1,
