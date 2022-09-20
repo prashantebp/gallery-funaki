94daf94bd11ff80aad7488402a7e9ab90993691c
rajan <r.silwal@andmine.com>
2022-09-20 11:43:37 +0545

    updated

--- assets/global.js
+++ assets/global.js
@@ -36,7 +36,7 @@ jQuery(document).ready(function(jQuery){
 console.log(jQuery('.product-slider-box').slick.slideCount);
 
     jQuery('.product-slider-box').on('init', function(event, slick){
-      console.log($(this).slideCount);
+      jQuery(this).append('+slick.slideCount+');
     });
 
     jQuery('.product-slider-box').slick({
